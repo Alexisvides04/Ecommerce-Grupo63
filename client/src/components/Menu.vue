1c3fa23d8a3d9a22ec40ac797ec2567fb24ad85d
Juan David Triana Turmeque <88797895+JuanDavidTriana@users.noreply.github.com>
2021-10-01 12:07:29 -0500

    Add files via upload

---
+++ client/src/components/Menu.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="ui secondary menu">
+    <div class="ui container">
+      <div class="left menu">
+        <router-link class="item" to="/">
+          <img class="ui small image" src="../assets/logo.png" alt="Ecomerce" />
+          <p>Categorias...</p>
+        </router-link>
+      </div>
+      <!-- Iniciar sesion -->
+      <div class="right menu">
+        <router-link class="item" to="/login" v-if="!token">
+          Iniciar sesión
+        </router-link>
+
+        <template v-if="token">
+          <router-link class="item" to="/orders">Pedidos</router-link>
+          <span class="ui item cart">
+            <i class="shopping cart icon"></i>
+          </span>
+          <span class="ui item logout">
+            <i class="sign-out icon"></i>
+          </span>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getTokenApi } from '../api/token';
+export default {
+  name: 'Menu',
+
+  setup() {
+    const token = getTokenApi();
+
+    return {
+      token,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.ui.menu.secondary {
+  background-color: #16202b;
+
+  .item {
+    color: #ffffff;
+    &:hover {
+      color: #1fa1f1;
+    }
+  }
+
+  .menu.left {
+    width: 50%;
+    .ui.image {
+      width: 120px;
+    }
+  }
+
+  .menu.right {
+    width: 50%;
+    justify-content: flex-end;
+
+    .logout,
+    .cart {
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+}
+</style>

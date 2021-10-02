ff2ea545aa32b79eea1a33115fa221b62165e621
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-02 11:00:15 -0500

    server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8da351986cb72386d7809f312e0ab7be'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23fbfbdb-c3e9-4549-89ec-197ca0d09810'
+};

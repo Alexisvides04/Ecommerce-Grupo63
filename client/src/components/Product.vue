bba79b6b2703249b38ea5d7fa1bf4deb3bfbb54a
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-04 13:50:05 -0500

    Renderizar todos los productos

---
+++ client/src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="ui card product">
+    <div class="image">
+      <img :src="API_URL + product.image.url" :alt="product.name" />
+    </div>
+    <div class="content">
+      <div class="header">{{ product.name }}</div>
+      <div class="description">{{ product.price }} $</div>
+    </div>
+    <div class="ui button primary" @click="addProductCart(product.id)">
+      Comprar
+    </div>
+  </div>
+</template>
+
+<script>
+import { API_URL } from '../utils/constants';
+export default {
+  name: 'Product',
+  props: {
+    product: Object,
+  },
+  setup(props) {
+    console.log(props.product);
+
+    return {
+      API_URL,
+    };
+  },
+};
+</script>
+
+<style></style>

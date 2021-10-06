1aa9349ff3123a7b8f704fe75520f1a585e5606b
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-06 08:57:20 -0500

    Creando carrito y abiendolo

--- client/src/layouts/BasicLayouts.vue
+++ client/src/layouts/BasicLayouts.vue
@@ -5,15 +5,19 @@
       <slot />
     </div>
   </div>
+
+  <Cart />
 </template>
 
 <script>
 import Menu from '../components/Menu.vue';
+import Cart from '../components/Cart/Cart.vue';
 export default {
   name: 'BasicLayouts',
 
   components: {
     Menu,
+    Cart,
   },
 };
 </script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,8 +2,13 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    showCart: false,
   },
   mutations: {
+    setShowCart(state,payload){
+      state.showCart = payload;
+    }
+
   },
   actions: {
   },

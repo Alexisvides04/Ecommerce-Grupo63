f46af2a8802bfcaaff3a484328aa34580a1b4971
JuanDavidTriana <juandavidtriana.jdt@gmail.com>
2021-10-07 07:18:01 -0500

    CartFooter

---
+++ client/src/views/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+  <BasicLayouts>
+    <h1>Carrito</h1>
+  </BasicLayouts>
+</template>
+
+<script>
+import BasicLayouts from '../layouts/BasicLayouts.vue';
+export default {
+  name: 'Cart',
+  components: {
+    BasicLayouts,
+  },
+};
+</script>
+
+<style></style>
